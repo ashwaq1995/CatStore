2e45330f269c551bafc55f9e56548eabb4f747e0
Ashwaq Alharbi <ashwaq.alharbi.19@hotmail.com>
2022-06-21 10:46:57 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+// import cat1 from "./public/images/cat1.jfif";
+
+import cat1 from "./images/cat1.jfif";
+import cat2 from "./images/cat2.jfif";
+import cat3 from "./images/cat3.jfif";
+import cat4 from "./images/cat4.jfif";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <h1>Cat Store</h1>
+ 
+    <div className='position'>
+
+  <div>
+  <img src={cat1} className="img" />
+  <h3>name:cat1</h3>
+  <h5>Age:3 months</h5>
+  </div>
+
+  <div>
+  <img src={cat2} className="img" />
+  <h3>name:cat2</h3>
+  <h5>Age:4 months</h5>
+  </div>
+
+  <div>
+  <img src={cat3} className="img" />
+  <h3>name:cat3</h3>
+  <h5>Age:3 months</h5>
+  </div>
+
+  <div>
+  <img src={cat4} className="img" />
+  <h3>name:cat4</h3>
+  <h5>Age:2 months</h5>
+  </div>
+  </div>
+
+  </div>
   );
 }
 
